feecdf48c3fd3350faabf17341874617df815f93
Härtl, Manuel <manuel.haertl@student.fhws.de>
2022-06-23 16:30:24 +0200

    added startscreen

--- app/src/main/java/com/example/vertiefungqrvisitenkartenapp/MainActivity.kt
+++ app/src/main/java/com/example/vertiefungqrvisitenkartenapp/MainActivity.kt
@@ -1,71 +1,92 @@
 package com.example.vertiefungqrvisitenkartenapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
 import com.budiyev.android.codescanner.DecodeCallback
 import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
+
+    private lateinit var codeScanner: CodeScanner
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       setContentView(R.layout.activity_main)
+//startQrCodeScanner()
+
+    }
 
 
+    private fun startQrCodeScanner() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                123
+            )
+        } else {
+            startScanning()
+        }
+    }
 
+    private fun startScanning() {
+        setContentView(R.layout.qr_code_scanner)
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
 
+        codeScanner = CodeScanner(this, scannerView)
 
-        private lateinit var codeScanner: CodeScanner
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
-
-            codeScanner = CodeScanner(this, scannerView)
-
-            // Parameters (default values)
-            codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
-            codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
-            // ex. listOf(BarcodeFormat.QR_CODE)
-            codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
-            codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
-            codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
-            codeScanner.isFlashEnabled = false // Whether to enable flash or not
-
-            // Callbacks
-            codeScanner.decodeCallback = DecodeCallback {
-                runOnUiThread {
-                    Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
-                }
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
             }
-            codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
-                runOnUiThread {
-                    Toast.makeText(this, "Camera initialization error: ${it.message}",
-                        Toast.LENGTH_LONG).show()
-                }
-            }
-
-            scannerView.setOnClickListener {
-                codeScanner.startPreview()
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(
+                    this, "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
 
-        override fun onResume() {
-            super.onResume()
+        scannerView.setOnClickListener {
             codeScanner.startPreview()
         }
 
-        override fun onPause() {
-            codeScanner.releaseResources()
-            super.onPause()
-        }
     }
+
+    override fun onResume() {
+        super.onResume()
+//        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+//        codeScanner.releaseResources()
+        super.onPause()
+    }
+}
 
